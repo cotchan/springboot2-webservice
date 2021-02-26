18cb89e2cf299d8c7446baf39dfd09cd02d3b755
cotchan <zhtcks@gmail.com>
2021-02-27 03:13:16 +0900

    등록/수정/조회 API 만들기

---
+++ src/main/java/com/cotchan/springboot/service/post/PostsService.java
@@ -0,0 +1,40 @@
+package com.cotchan.springboot.service.post;
+
+import com.cotchan.springboot.domain.post.Posts;
+import com.cotchan.springboot.domain.post.PostsRepository;
+import com.cotchan.springboot.web.dto.PostsResponseDto;
+import com.cotchan.springboot.web.dto.PostsSaveRequestDto;
+import com.cotchan.springboot.web.dto.PostsUpdateRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id = " + id));
+
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+
+        return id;
+    }
+
+    public PostsResponseDto findById(Long id) {
+        Posts entity = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id = " + id));
+        return new PostsResponseDto(entity);
+    }
+
+
+}
